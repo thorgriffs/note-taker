e7dca110c797a50f812962588a0af02062348da7
thorgriffs <22.kelliking@gmail.com>
2021-01-10 00:05:44 -0500

    setup express app and handle data parsing, added basic routes on server.js, corrected routes to css and index.html on notes.html

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+
+
+var app = express();
+var PORT = process.env.port || 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+  
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "notes.html"));
+  });
